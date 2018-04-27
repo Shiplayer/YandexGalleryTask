b581e7d5b751ccf8a33c395227b616d518e004b4
Shiplayer <shiplayer@yandex.ru>
2018-04-28 00:15:28 +0300

    add class for handle json response, add ViewModel for getting response

---
+++ app/src/main/java/com/developer/java/yandex/yandexgallerytask/entity/PhotoResponse.java
@@ -0,0 +1,21 @@
+package com.developer.java.yandex.yandexgallerytask.entity;
+
+
+public class PhotoResponse {
+    public String file;
+    public String name;
+    public String preview;
+    public String path;
+    public String mime_type;
+
+    @Override
+    public String toString() {
+        return "PhotoResponse{" +
+                "file='" + file + '\'' +
+                ", name='" + name + '\'' +
+                ", preview='" + preview + '\'' +
+                ", path='" + path + '\'' +
+                ", mime_type='" + mime_type + '\'' +
+                '}';
+    }
+}
