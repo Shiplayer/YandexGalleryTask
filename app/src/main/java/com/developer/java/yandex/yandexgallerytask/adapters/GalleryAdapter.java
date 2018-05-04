525da77c5252894394d90fe964fe038b36891fa7
Shiplayer <shiplayer@yandex.ru>
2018-05-04 12:49:57 +0300

    add startActivity for transition to fullscreen activity, update layout for CardView, add new dependence

--- app/src/main/java/com/developer/java/yandex/yandexgallerytask/adapters/GalleryAdapter.java
+++ app/src/main/java/com/developer/java/yandex/yandexgallerytask/adapters/GalleryAdapter.java
@@ -3,8 +3,8 @@ package com.developer.java.yandex.yandexgallerytask.adapters;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.Observer;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
-import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -12,22 +12,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
+import com.developer.java.yandex.yandexgallerytask.ImageViewActivity;
 import com.developer.java.yandex.yandexgallerytask.R;
 import com.developer.java.yandex.yandexgallerytask.entity.PhotoResponse;
-import com.developer.java.yandex.yandexgallerytask.net.ImageLoader;
 import com.developer.java.yandex.yandexgallerytask.net.YandexCommunication;
-import com.developer.java.yandex.yandexgallerytask.util.ExecutorUtil;
-import com.squareup.picasso.Downloader;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.OkHttp3Downloader;
 import com.squareup.picasso.Picasso;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ThreadPoolExecutor;
-
-import javax.net.ssl.HttpsURLConnection;
 
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
@@ -44,9 +41,11 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
     private String auth;
     private OkHttpClient client;
     private Picasso picasso;
+    private Context context;
 
     public GalleryAdapter(Context context, final String auth){
         this.auth = auth;
+        this.context = context;
         client = new OkHttpClient.Builder().addInterceptor(new Interceptor() {
             @Override
             public Response intercept(Chain chain) throws IOException {
@@ -66,7 +65,26 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
     public void onBindViewHolder(final ViewHolder holder, final int position) {
         Log.w(TAG, "loading image " + urls.get(position).name);
         final LiveData<String> liveLink = YandexCommunication.getInstance().getLink(urls.get(position).path);
-        picasso.load(urls.get(position).preview).into(holder.imageView);
+        holder.progressBar.setVisibility(View.VISIBLE);
+        /*holder.imageView.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                holder.imageView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                picasso.load(urls.get(position).preview).resize(0, holder.imageView.getHeight()).into(holder.imageView);
+            }
+        });*/
+        picasso.load(urls.get(position).preview).fit().centerInside().into(holder.imageView, new Callback() {
+            @Override
+            public void onSuccess() {
+                holder.progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onError(Exception e) {
+                Log.e(TAG, e.getMessage());
+                e.printStackTrace();
+            }
+        });
 
         liveLink.observeForever(new Observer<String>() {
             @Override
@@ -76,29 +94,15 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
                 liveLink.removeObserver(this);
             }
         });
-        /*ExecutorUtil.THREAD_POOL_EXECUTOR.execute(new ImageLoader(urls.get(position).path, new OnStateListener() {
-            @Override
-            public void shutdown() {
-                Log.w(TAG, "process was interrupt");
-            }
 
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void downloading() {
-                Log.w(TAG, "downloading");
+            public void onClick(View view) {
+                Intent intent = new Intent(context, ImageViewActivity.class);
+                intent.putExtra("URL", urls.get(position).link);
+                context.startActivity(intent);
             }
-
-            @Override
-            public void downloaded() {
-                Log.w(TAG, "downloaded");
-            }
-
-            @Override
-            public void setResultIntoView(Bitmap bitmap) {
-                holder.imageView.setImageBitmap(bitmap);
-                Log.w(TAG, "inserting!");
-            }
-        }, auth));*/
-        //Picasso.get().load(urls.get(position).preview).into(holder.imageView);
+        });
     }
 
     @Override
@@ -107,10 +111,12 @@ public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHold
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        public ProgressBar progressBar;
         public ImageView imageView;
         public ViewHolder(View itemView) {
             super(itemView);
             imageView = itemView.findViewById(R.id.imageView);
+            progressBar = itemView.findViewById(R.id.pb_adaptor_element);
 
         }
     }
